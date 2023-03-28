e6b4f5bee6d6635d4392a23bdc3154ef9c47d023
Mark Khramov <hramov.mark321@gmail.com>
2023-03-28 19:58:45 +0300

    created few components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Technologies from "./components/Technologies";
+import Header from "./components/Header";
+
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
 }
 
 export default App;
