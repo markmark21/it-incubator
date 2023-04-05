9dc1493e343277045c2b1b4e715034c0815b22b3
Mark Khramov <hramov.mark321@gmail.com>
2023-04-05 13:19:35 +0300

    updated classes

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,32 @@
 import React from "react";
-import s from './Dialogs.module.css'
+import s from "./Dialogs.module.css";
+
 const Dialogs = () => {
     return (
-        <div className={s.content}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Lena
+                </div>
+                <div className={s.dialog}>
+                    Dasha
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Mark
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How are you</div>
+                <div className={s.dialog}>Yo</div>
+            </div>
         </div>
     );
 };
