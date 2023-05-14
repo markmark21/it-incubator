cf8dfcc90bbed67bafde73a99aaded56c0bcbe22
Mark Khramov <hramov.mark321@gmail.com>
2023-05-14 22:22:40 +0300

    state

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,7 @@ import {BrowserRouter, Route} from "react-router-dom";
 import {MyPostsPropsType} from "./components/Profile/MyPosts/MyPosts";
 
 
-function App(props: MyPostsPropsType) {
-    let dialogsData = [
-        {id: 1, name: "Dima"},
-        {id: 2, name: "Lena"},
-        {id: 3, name: "Dasha"},
-        {id: 4, name: "Andrey"},
-        {id: 5, name: "Mark"},
-        {id: 6, name: "Valera"}
-    ];
-    let messagesData = [
-        {id: 1, message: "Hi"},
-        {id: 2, message: "How are you"},
-        {id: 3, message: "Yo"},
-        {id: 4, message: "Yo"},
-        {id: 5, message: "Yo"}
-    ];
+function App(props: any) {
 
     return (
         <div className="app-wrapper">
@@ -31,11 +16,8 @@ function App(props: MyPostsPropsType) {
                 <Header/>
                 <Navbar/>
                 <div className="app-wrapper-content">
-                    {/*<Route exact path="/dialogs" component={Dialogs}/>
-                    <Route exact path="/profile" component={Profile}/>*/}
-                    
-                    <Route exact path="/dialogs" render={()=> <Dialogs dialogs={dialogsData} messages={messagesData}/>}/>
-                    <Route exact path="/profile" render={()=> <Profile posts={props.posts} />}/>
+                    <Route exact path="/dialogs" render={()=> <Dialogs dialogs={props.appState.dialogs} messages={props.appState.messages}/>}/>
+                    <Route exact path="/profile" render={()=> <Profile posts={props.appState.posts} />}/>
 
                 </div>
             </BrowserRouter>
--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,14 @@ import "./index.css";
 import App from "./App";
 import {DevSupport} from "@react-buddy/ide-toolbox";
 import {ComponentPreviews, useInitial} from "./dev";
+import state from "./redux/state";
 
-let postsData = [
-    {id: 1, message: 'Hi, how are you?', likesCount: 12},
-    {id: 2, message: 'My fist post', likesCount: 11},
-    {id: 3, message: 'Blabla', likesCount: 11},
-    {id: 4, message: 'Dada', likesCount: 11},
-]
 
 ReactDOM.render(
     <DevSupport ComponentPreviews={ComponentPreviews}
                 useInitialHook={useInitial}
     >
-        <App posts={postsData}/>
+        <App appState={state}/>
     </DevSupport>,
     document.getElementById("root")
 );
