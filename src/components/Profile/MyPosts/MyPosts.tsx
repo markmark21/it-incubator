e084d51dfa0a61a59b1b9223bf6043e7c8e88952
Mark Khramov <hramov.mark321@gmail.com>
2023-05-22 18:25:54 +0300

    state type

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -13,16 +13,21 @@ export type MyPostsPropsType = {
 }
 
 const MyPosts = (props: MyPostsPropsType) => {
+    let newPostElement = React.createRef()
+    const addPost = () => {
+      let text = newPostElement.current.value
+    }
+
     return (
         <div className={s.postsBlock}>
             <div className="posts">
                 <h3>My posts</h3>
                 <div>
                     <div>
-                        <textarea></textarea>
+                        <textarea ref={newPostElement}></textarea>
                     </div>
                     <div>
-                        <button onClick={ ()=> {alert('hey')} }>Add post</button>
+                        <button onClick={ addPost }>Add post</button>
                     </div>
                 </div>
             </div>
