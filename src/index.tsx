feace3f120edd5f404156d33185c834308708373
Mark Khramov <hramov.mark321@gmail.com>
2023-05-25 13:49:04 +0300

    rerender.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
-import state, {addPost} from "./redux/state";
+import {rerenderEntireTree} from "./render";
+import state from "./redux/state";
 
-
-ReactDOM.render(
-    <DevSupport ComponentPreviews={ComponentPreviews}
-                useInitialHook={useInitial}
-    >
-        <App state={state} addPost={addPost}/>
-    </DevSupport>,
-    document.getElementById("root")
-);
+rerenderEntireTree(state);
